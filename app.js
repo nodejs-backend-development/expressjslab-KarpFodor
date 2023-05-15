74c772659907b28e1c0f835b9ae4e662114ece2c
Time Play <timeplay973@gmail.com>
2023-05-15 21:13:32 +0300

    Lab_4

--- app.js
+++ app.js
@@ -28,13 +28,16 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
     // set locals, only providing error in development
-    res.locals.message = err.message;
+
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
+    if (err.status === 404) {
+        res.locals.message = 'Page not found. 404';
+    }
     res.render('error');
 });
 
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,36 +1,77 @@
-const users = [
-    {
-        id: 1,
-        firstName: 'FirstName1',
-        lastName: 'LastName1',
-        email: 'a1@b.com',
-    },
-    {
-        id: 2,
-        firstName: 'FirstName2',
-        lastName: 'LastName2',
-        email: 'a2@b.com',
-    },
-    {
-        id: 3,
-        firstName: 'FirstName3',
-        lastName: 'LastName3',
-        email: 'a3@b.com',
-    },
-];
+const { makeRequest } = require('../clients/httpClient');
 
 const getUsers = async (req, res) => {
-    res.status(200).json(users);
+    const id = req.params.userID === undefined ? '' : req.params.userID;
+    const token = req.headers.authorization;
+
+    try {
+        const userData = await makeRequest(
+            `https://gorest.co.in/public/v2/users/${id}`,
+            'GET',
+            token,
+        );
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).send('Error getting user with id');
+    }
 };
 
 const addUser = async (req, res) => {
+    const token = req.headers.authorization;
     const user = req.body;
-    user.id = users.length + 1;
-    users.push(user);
-    res.status(201).json(user);
+
+    try {
+        const { name, email, gender, status } = req.body;
+        const userData = await makeRequest('https://gorest.co.in/public/v2/users/', 'POST', token, {
+            name,
+            email,
+            gender,
+            status,
+        });
+        res.status(201).json(userData);
+    } catch (error) {
+        res.status(500).send('Error creating user');
+    }
+};
+
+const delUser = async (req, res) => {
+    const id = req.params.userID === undefined ? '' : req.params.userID;
+    const token = req.headers.authorization;
+
+    try {
+        await makeRequest(`https://gorest.co.in/public/v2/users/${id}`, 'DELETE', token);
+        res.status(202).send('User deleted');
+    } catch (error) {
+        res.status(500).send('Error deleting user');
+    }
+};
+
+const updUser = async (req, res) => {
+    const id = req.params.userID === undefined ? '' : req.params.userID;
+    const token = req.headers.authorization;
+
+    try {
+        const { name, email, gender, status } = req.body;
+        const userData = await makeRequest(
+            `https://gorest.co.in/public/v2/users/${id}`,
+            'PUT',
+            token,
+            {
+                name,
+                email,
+                gender,
+                status,
+            },
+        );
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).send('Error updating user with id');
+    }
 };
 
 module.exports = {
     getUsers,
     addUser,
+    delUser,
+    updUser,
 };
